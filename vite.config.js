d395ddfce4c86c7fff0ce9192606038bad1a30aa
salahuddingfx <salauddinkaderappy@gmail.com>
2026-05-31 09:20:57 +0600

    Configure development server port to 5173

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173
+  },
   resolve: {
     alias: {
       "@sirat/ui": fileURLToPath(new URL("./src/lib/ui/index.js", import.meta.url)),
